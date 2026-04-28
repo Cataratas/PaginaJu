0ad882ce72265671d4170951c5bf5c4b718f2609
Cataratas <cataratasdolly@outlook.com>
2026-04-28 19:43:07 -0300

    video fix

--- js/videos.js
+++ js/videos.js
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', () => {
             const videoSrc = container.getAttribute('data-video');
             container.innerHTML = `
                 <video autoplay controlsList="nodownload noremoteplayback">
-                    <source src="${videoSrc}" type="video/mp4; codecs=hvc1">
+                    <source src="${videoSrc}" type="video/mp4">
                 </video>
                 <span class="play-btn"></span>
                 <div class="controls">
