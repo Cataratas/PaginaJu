cd4a79226237322d243198e68b5758198abda40a
Cataratas <cataratasdolly@outlook.com>
2026-04-28 11:53:46 -0300

    Video icons

--- js/videos.js
+++ js/videos.js
@@ -7,14 +7,56 @@ document.addEventListener('DOMContentLoaded', () => {
 
             const videoSrc = container.getAttribute('data-video');
             container.innerHTML = `
-                <video autoplay controls>
+                <video autoplay>
                     <source src="${videoSrc}" type="video/mp4">
                 </video>
+                <span class="play-btn"></span>
+                <div class="controls">
+                    <div class="progress-bar">
+                        <div class="progress"></div>
+                    </div>
+                </div>
             `;
 
             const video = container.querySelector('video');
+            const progress = document.querySelector(".progress");
+            const progressBar = document.querySelector(".progress-bar");
+            const btn = container.querySelector(".play-btn");
+
+            btn.classList.add("play");
+
             video.addEventListener('ended', () => {
                 container.innerHTML = originalContent;
+            });
+            video.addEventListener("timeupdate", () => {
+                const percent = (video.currentTime / video.duration) * 100;
+                progress.style.width = percent + "%";
+            });
+            progressBar.addEventListener("click", (e) => {
+                const rect = progressBar.getBoundingClientRect();
+                const clickX = e.clientX - rect.left;
+                const width = rect.width;
+
+                const percent = clickX / width;
+                video.currentTime = percent * video.duration;
+            });
+            video.addEventListener("click", () => {
+                if (video.paused) {
+                    video.play().then(() => {});
+                    btn.classList.remove("play", "pause")
+                } else {
+                    video.pause();
+                    btn.classList.remove("play")
+                    btn.classList.add("pause")
+                }
+            });
+            video.addEventListener("play", () => {
+                btn.style.opacity = "0";
+            });
+            video.addEventListener("pause", () => {
+                btn.style.opacity = "1";
+                btn.classList.remove("play");
+                btn.classList.add("pause");
             });
         });
     });
